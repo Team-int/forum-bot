b54fdf6001b5038da4c2a71fae64312b52168a12
mswgen <54498184+mswgen@users.noreply.github.com>
2020-12-12 15:02:06 +0900

    asdf

--- assets/js/loadingbar.js
+++ assets/js/loadingbar.js
@@ -2,3 +2,4 @@ window.addEventListener('load', function () {
     document.querySelector('#loadingbar').style.display = 'none';
     document.querySelector('#main').style.display = 'block';
 })
+console.error('https://discord.gg/gF4ZbnkKhD');
