ec230d593d0f7a050f3ab218897120a761b0fb5e
mswgen <54498184+mswgen@users.noreply.github.com>
2021-02-01 12:29:24 +0900

    .

--- commands/restore.js
+++ commands/restore.js
@@ -12,34 +12,32 @@ module.exports = {
         .setTimestamp()
         .setFooter(message.author.tag, message.author.displayAvatarURL());
         let m = await message.channel.send(embed);
-        await m.react('✅');
-        await m.react('❌');
         const filter = (r, u) => (r.emoji.name == '✅' || r.emoji.name == '❌') && u.id == message.author.id;
-        const collector = m.createReactionCollector(filter, {
+        const collector = message.channel.createReactionCollector(filter, {
             max: 1
         });
-        collector.on('end', async collected => {
+        collector.on('end', collected => {
             if (collected.first().emoji.name == '✅') {
-                await message.guild.channels.cache.forEach(async x => await x.delete());
-                await message.guild.roles.cache.forEach(async x => await x.delete());
+                message.guild.channels.cache.forEach(x => x.delete());
+                message.guild.roles.cache.forEach(x => x.delete());
                 let backupFile = require('/home/data/backup.json');
-                await message.guild.setName(backupFile.name);
-                await message.guild.setIcon(backupFile.icon);
-                await message.guild.setRegion(backupFile.region);
-                await message.guild.setDefaultMessageNotifications(backupFile.notify);
+                message.guild.setName(backupFile.name);
+                message.guild.setIcon(backupFile.icon);
+                message.guild.setRegion(backupFile.region);
+                message.guild.setDefaultMessageNotifications(backupFile.notify);
                 for (let r of backupFile.roles) {
-                    await message.guild.roles.create({data: r});
+                    message.guild.roles.create({data: r});
                 }
                 for (let e of backupFile.emojis) {
-                    await message.guild.emojis.create(e.url, e.name);
+                    message.guild.emojis.create(e.url, e.name);
                 }
-                await message.guild.setVerificationLevel(backupFile.verifyLevel);
-                await message.guild.setExplicitContentFilter(backupFile.media);
+                message.guild.setVerificationLevel(backupFile.verifyLevel);
+                message.guild.setExplicitContentFilter(backupFile.media);
                 for (let b of backupFile.bans) {
-                    await message.guild.members.ban(b);
+                    message.guild.members.ban(b);
                 }
                 for (let c of backupFile.channels) {
-                    await message.guild.channels.create(c.name, {
+                    message.guild.channels.create(c.name, {
                         type: c.type,
                         topic: c.topic,
                         nsfw: c.nsfw,
@@ -51,11 +49,10 @@ module.exports = {
                         rateLimitPerUser: c.slow
                     });
                 }
-                await message.guild.setAFKChannel(backupFile.afkCh);
-                await message.guild.setAFKTimeout(backupFile.afkTime);
-                await message.guild.setSystemChannel(backupFile.sysCh);
-                await message.guild.setSystemChannelFlags(backupFile.sysMsg);
-                message.author.send('서버 복원 완료!');
+                message.guild.setAFKChannel(backupFile.afkCh);
+                message.guild.setAFKTimeout(backupFile.afkTime);
+                message.guild.setSystemChannel(backupFile.sysCh);
+                message.guild.setSystemChannelFlags(backupFile.sysMsg);
             }
         });
     }
